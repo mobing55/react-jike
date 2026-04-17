bd53748b694daf34fcf36460b67c8e8ae99c031f
yuyiheng <2893504461@qq.com>
2026-04-17 09:17:58 +0800

    依赖安装

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Button } from "antd"
+
 function App() {
-  return <div>this is app</div>
+  return (
+    <div>
+      this is app
+      <Button type='primary'>test</Button>
+    </div>
+  ) 
 }
 
 export default App
