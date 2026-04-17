1a4a086ba089f87853d5b7077c7b3f41dbe5aae4
yuyiheng <2893504461@qq.com>
2026-04-17 10:31:24 +0800

    表单数据收集

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,16 +3,38 @@ import { Card, Form, Input, Button } from 'antd'
 import logo from '@/assets/logo.png'
 
 const Login = () => {
+  const onFinish = values => {
+    console.log(values);
+    
+  }
   return (
     <div className="login">
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
-        <Form>
-          <Form.Item>
+        <Form onFinish={onFinish} validateTrigger = 'onBlur'>
+          <Form.Item
+          name="mobile"
+          rules={[
+              { 
+                required: true, 
+                message: '请输入手机号!' 
+              },
+              { 
+                pattern: /^1[3-9]\d{9}$/,
+                message: '请输入正确的手机号格式'
+              }
+            ]}>
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
+          <Form.Item
+          name="code"
+          rules={[
+              { 
+                required: true, 
+                message: '请输入验证码!' 
+              }
+            ]}>
             <Input size="large" placeholder="请输入验证码" />
           </Form.Item>
           <Form.Item>
