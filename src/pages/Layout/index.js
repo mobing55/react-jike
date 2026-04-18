810759f7175d69c0ae6b64c78368c8f2ac92f191
yuyiheng <2893504461@qq.com>
2026-04-18 15:22:10 +0800

    根据路由路径高亮菜单

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -6,7 +6,7 @@ import {
   LogoutOutlined,
 } from '@ant-design/icons'
 import './index.scss'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 const { Header, Sider } = Layout
 
@@ -35,6 +35,13 @@ const GeekLayout = () => {
     const path = route.key
     navigate(path)
   }
+
+  // 反向高亮
+  // 1.获取当前路由路径
+  const location = useLocation()
+  // console.log(location.pathname);
+  const selectedKey = location.pathname
+  
   return (
     <Layout>
       <Header className="header">
@@ -53,7 +60,7 @@ const GeekLayout = () => {
           <Menu
             mode="inline"
             theme="dark"
-            defaultSelectedKeys={['1']}
+            selectedKeys={selectedKey}
             onClick={onMenuClick}
             items={items}
             style={{ height: '100%', borderRight: 0 }}></Menu>
