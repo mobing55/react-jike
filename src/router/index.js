250df9fc31c18e1d32f09e81a3a06ea9a7478ee2
yuyiheng <2893504461@qq.com>
2026-04-18 15:15:32 +0800

    二级路由配置与点击跳转交互

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
+import Home from "@/pages/Home";
+import Article from "@/pages/Article";
+import Publish from "@/pages/Publish";
 import { AuthRoute } from "@/components/AuthRoute";
+
 // 配置路由
 const router = createBrowserRouter([
     {
         path:'/',
-        element: <AuthRoute> <Layout /> </AuthRoute>
+        element: <AuthRoute> <Layout /> </AuthRoute>,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: '/article',
+                element: <Article />
+            },
+            {
+                path: '/publish',
+                element: <Publish />
+            },
+        ]
     },
     {
         path:'/login',
