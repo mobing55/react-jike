d7aba1316d6f0436a6a689111c9efc04b31033ff
yuyiheng <2893504461@qq.com>
2026-04-19 16:31:16 +0800

    基础table的渲染

--- src/apis/article.js
+++ src/apis/article.js
@@ -17,3 +17,12 @@ export function createArticleAPI(data) {
         data
     })
 }
+
+// 获取文章列表
+export function getArticleAPI(params) {
+    return request({
+        url:'/mp/articles',
+        method: 'GET',
+        params
+    })
+}
--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -8,6 +8,8 @@ import { Table, Tag, Space } from 'antd'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import img404 from '@/assets/error.png'
 import { useChannel } from '@/hooks/useChannel'
+import { useEffect, useState } from 'react'
+import { getArticleAPI } from '@/apis/article'
 
 const { Option } = Select
 const { RangePicker } = DatePicker
@@ -82,6 +84,19 @@ const Article = () => {
         title: 'wkwebview离线化加载h5资源解决方案'
         }
     ]
+
+    // 获取文章列表
+    const [list, setList] = useState([])
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+        async function getList() {
+            const res = await getArticleAPI()
+            setList(res.data.results)
+            setCount(res.data.total_count)
+        }
+        getList()
+    },[])
+
     return (
         <div>
         <Card
@@ -125,8 +140,8 @@ const Article = () => {
             </Form>
         </Card>
         {/* 表格区域 */}
-        <Card title={`根据筛选条件共查询到 count 条结果：`}>
-            <Table rowKey="id" columns={columns} dataSource={data} />
+        <Card title={`根据筛选条件共查询到 ${count} 条结果：`}>
+            <Table rowKey="id" columns={columns} dataSource={list} />
         </Card>
         </div>
     )
