5755541fe2857ddf001839a618a4f5666be3bdff
yuyiheng <2893504461@qq.com>
2026-04-20 15:03:27 +0800

    文章列表分页逻辑实现

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -120,6 +120,16 @@ const Article = () => {
         })
     }
 
+    // 分页
+    const onPageChange = (page) => {
+        console.log(page);
+        // 修改参数依赖项，引发数据重新获取列表渲染
+        setReqData({
+            ...reqData,
+            page
+        })
+    }
+
     return (
         <div>
         <Card
@@ -164,7 +174,11 @@ const Article = () => {
         </Card>
         {/* 表格区域 */}
         <Card title={`根据筛选条件共查询到 ${count} 条结果：`}>
-            <Table rowKey="id" columns={columns} dataSource={list} />
+            <Table rowKey="id" columns={columns} dataSource={list} pagination={{
+                total: count,
+                pageSize: reqData.per_page,
+                onChange: onPageChange,
+            }}/>
         </Card>
         </div>
     )
