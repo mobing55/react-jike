85ff08297a6cb21d5f754e34c35bf788a2b91bf1
yuyiheng <2893504461@qq.com>
2026-04-20 19:06:41 +0800

    基础数据回填

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -11,13 +11,13 @@ import {
   message
 } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import './index.scss'
 
 import ReactQuill from 'react-quill-new'
 import 'react-quill-new/dist/quill.snow.css'
-import { useState } from 'react'
-import { createArticleAPI } from '@/apis/article'
+import { useEffect, useState } from 'react'
+import { createArticleAPI, getArticleById } from '@/apis/article'
 import { useChannel } from '@/hooks/useChannel'
 
 const { Option } = Select
@@ -59,6 +59,22 @@ const Publish = () => {
     // console.log('切换封面',e.target.value);
     setImageType(e.target.value)
   }
+
+  // 回填数据
+  const [searchParams] = useSearchParams()
+  const articleId = searchParams.get('id')
+  // 获取实例
+  const [form] = Form.useForm()
+  useEffect(() => {
+    // 1.通过id获取数据
+    async function getArticleDetail() {
+      const res = await getArticleById(articleId)
+      form.setFieldsValue(res.data)
+    }
+    getArticleDetail()
+    // 2.调用实例方法 完成回填
+    
+  },[articleId, form])
   return (
     <div className="publish">
       <Card
@@ -75,6 +91,7 @@ const Publish = () => {
           wrapperCol={{ span: 16 }}
           initialValues={{ type: 1 }}
           onFinish={onFinish}
+          form={form}
         >
           <Form.Item
             label="标题"
